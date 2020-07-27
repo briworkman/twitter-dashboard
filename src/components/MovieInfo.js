b0d1105035c03c232feaf7da45ea625e3dc44e30
Brianna Workman <brianna.workman@hotmail.com>
2020-07-26 20:45:08 -0400

    refactors movie info styles

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -18,68 +18,6 @@ const StyledMovieInfo = styled.div`
   box-sizing: border-box;
   animation: animateMovieinfo 1s;
 
-  .movieinfo-content {
-    width: 85%;
-    max-width: 1280px;
-    min-height: 450px;
-    margin: 0 auto;
-    background: rgb(0, 0, 0, 0.7);
-    border-radius: 20px;
-    position: relative;
-  }
-  .movieinfo-thumb {
-    width: 300px;
-    float: left;
-    @media screen and (max-width: 768px) {
-      width: 100% !important;
-    }
-  }
-  .movieinfo-text {
-    font-family: Arial, Helvetica, sans-serif;
-    padding: 40px;
-    color: #fff;
-    overflow: hidden;
-    h1 {
-      font-family: 'Abel', sans-serif;
-      font-size: 48px;
-      margin: 0;
-      @media screen and (max-width: 1000px) {
-        font-size: 32px !important;
-      }
-    }
-    h3 {
-      font-size: 16px;
-      line-height: 0;
-      margin-top: 30px;
-    }
-    p {
-      font-family: 'Abel', sans-serif;
-      font-size: 18px;
-      line-height: 26px;
-    }
-  }
-  .rating-genre {
-    display: flex;
-    justify-content: flex-start;
-  }
-  .score {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 35px;
-    height: 35px;
-    background: #fff;
-    color: #000;
-    font-weight: 800;
-    border-radius: 25px;
-    margin: 0px 0 0 0;
-  }
-  .genre {
-    margin: 0 0 0 40px;
-    p {
-      margin: 0;
-    }
-  }
   @media screen and (max-width: 768px) {
     min-height: 600px;
     height: auto;

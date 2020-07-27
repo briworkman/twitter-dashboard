2ef323ca20d6b6ea88f572cc546b2a19f2b91731
Brianna Workman <brianna.workman@hotmail.com>
2020-07-26 20:31:46 -0400

    movie info bar styling complete

--- src/components/MovieInfoBar.js
+++ src/components/MovieInfoBar.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import clock from '../assets/clock-regular.svg';
+import money from '../assets/badge-dollar-regular.svg';
+import ticket from '../assets/ticket-alt-regular.svg';
 
 import { calcTime, convertMoney } from '../utils/helpers';
 
 function MovieInfoBar({ time, budget, revenue }) {
   return (
-    <div>
+    <div className='movieinfobar-container'>
       <div className='movieinfobar-content'>
         <div className='movieinfobar-content-col'>
-          {/* <FontAwesome className='fa-time' name='clock-o' size='2x' /> */}
+          <img src={clock} alt='clock' className='fa-time' />
           <span className='movieinfobar-info'>
             Running Time: {calcTime(time)}
           </span>
         </div>
 
         <div className='movieinfobar-content-col'>
-          {/* <FontAwesome className='fa-budget' name='money' size='2x' /> */}
+          <img src={money} alt='money' className='fa-budget' />
           <span className='movieinfobar-info'>
             Budget: {convertMoney(budget)}
           </span>
         </div>
         <div className='movieinfobar-content-col'>
-          {/* <FontAwesome className='fa-revenue' name='ticket' size='2x' /> */}
+          <img src={ticket} alt='ticket' className='fa-revenue' />
           <span className='movieinfobar-info'>
             Revenue: {convertMoney(revenue)}
           </span>

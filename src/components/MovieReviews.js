5ecec952ba20681a69def04f9c50a931faa410f7
Brianna Workman <brianna.workman@hotmail.com>
2020-07-30 20:46:48 -0400

    styled movie reviews

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -10,23 +10,27 @@ function MovieReviews(props) {
   }, []);
 
   return (
-    <div>
-      <img src={reviews} alt='5 stars' />
-      {props.reviews.map((reviews) => {
-        return (
-          <div className='reviews' key={reviews.id}>
-            <h3>{reviews.author}</h3>
-            <p>
-              {reviews.content.length > 250
-                ? reviews.content.substring(0, 250, 3) + '...'
-                : reviews.content}
-            </p>
-            <a href={reviews.url}>
-              <p>read more</p>
-            </a>
-          </div>
-        );
-      })}
+    <div className='review-container'>
+      <div>
+        <img src={reviews} alt='5 stars' />
+      </div>
+      <div className='review-flex'>
+        {props.reviews.map((reviews) => {
+          return (
+            <div className='reviews' key={reviews.id}>
+              <h3>{reviews.author}</h3>
+              <p>
+                {reviews.content.length > 250
+                  ? reviews.content.substring(0, 250, 3) + '...'
+                  : reviews.content}
+              </p>
+              <a href={reviews.url} target='_blank' rel='noopener noreferrer'>
+                <p className='read-more'>READ MORE</p>
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }

d0abc165dc843bc4e2a6e8eeda17354334a190fe
Brianna Workman <brianna.workman@hotmail.com>
2020-07-30 20:14:29 -0400

    renders movie reviews

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,16 +1,32 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchReviews } from '../actions';
+import reviews from '../assets/reviews.svg';
 
 function MovieReviews(props) {
   useEffect(() => {
     props.fetchReviews(props.id);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  console.log(props.reviews);
+
   return (
     <div>
-      <h1>Movie Reviews</h1>
+      <img src={reviews} alt='5 stars' />
+      {props.reviews.map((reviews) => {
+        return (
+          <div className='reviews' key={reviews.id}>
+            <h3>{reviews.author}</h3>
+            <p>
+              {reviews.content.length > 250
+                ? reviews.content.substring(0, 250, 3) + '...'
+                : reviews.content}
+            </p>
+            <a href={reviews.url}>
+              <p>read more</p>
+            </a>
+          </div>
+        );
+      })}
     </div>
   );
 }

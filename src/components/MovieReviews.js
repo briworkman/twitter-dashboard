9353e1c83fcc1b374f6b1d8a81a41cb108855e3f
Brianna Workman <brianna.workman@hotmail.com>
2020-07-30 20:49:20 -0400

    null handling for no movie reviews

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -10,27 +10,35 @@ function MovieReviews(props) {
   }, []);
 
   return (
-    <div className='review-container'>
-      <div>
-        <img src={reviews} alt='5 stars' />
-      </div>
-      <div className='review-flex'>
-        {props.reviews.map((reviews) => {
-          return (
-            <div className='reviews' key={reviews.id}>
-              <h3>{reviews.author}</h3>
-              <p>
-                {reviews.content.length > 250
-                  ? reviews.content.substring(0, 250, 3) + '...'
-                  : reviews.content}
-              </p>
-              <a href={reviews.url} target='_blank' rel='noopener noreferrer'>
-                <p className='read-more'>READ MORE</p>
-              </a>
-            </div>
-          );
-        })}
-      </div>
+    <div>
+      {props.reviews.length > 0 ? (
+        <div className='review-container'>
+          <div>
+            <img src={reviews} alt='5 stars' />
+          </div>
+          <div className='review-flex'>
+            {props.reviews.map((reviews) => {
+              return (
+                <div className='reviews' key={reviews.id}>
+                  <h3>{reviews.author}</h3>
+                  <p>
+                    {reviews.content.length > 250
+                      ? reviews.content.substring(0, 250, 3) + '...'
+                      : reviews.content}
+                  </p>
+                  <a
+                    href={reviews.url}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <p className='read-more'>READ MORE</p>
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }

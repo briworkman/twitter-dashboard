cdf48868062dc4473200419509918e73d688b0f2
Brianna Workman <brianna.workman@hotmail.com>
2020-08-01 09:09:55 -0400

    adds routing to movie info pages from actor page and sets up component for full actor movie credits

--- src/components/ActorInfo.js
+++ src/components/ActorInfo.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import { fetchActorInfo, fetchActorMovies } from '../actions/actors';
 import { IMG_URL, POSTER_SIZE } from '../utils/config';
 import { format, calcAge } from '../utils/helpers';
+import ActorMovieList from './ActorMovieList';
+import { NavLink, Route } from 'react-router-dom';
+import MovieInfo from './MovieInfo';
 
 function ActorInfo(props) {
   let id = props.match.params.id;
@@ -15,13 +18,12 @@ function ActorInfo(props) {
   let actor = props.actor;
   let actor_movies = props.actor_movies;
 
+  console.log('ACTOR: ', actor);
   console.log('ACTOR MOVIES: ', actor_movies);
 
   let known_for = actor_movies.cast
     ? actor_movies.cast.filter((movie) => movie.vote_average >= 7)
     : [];
-
-  console.log(known_for);
 
   return (
     <div className='actor-container'>
@@ -41,10 +43,30 @@ function ActorInfo(props) {
           <div className='actor-movies'>
             {known_for.map((movies) => {
               return (
-                <div className='movie-list'>
-                  <img
-                    src={`${IMG_URL}${POSTER_SIZE}${movies.poster_path}`}
-                    alt='movie thumb'
+                <div>
+                  <NavLink
+                    to={{
+                      pathname: `/movie/${movies.id}`,
+                      props: { movies },
+                    }}
+                  >
+                    <div key={movies.id}>
+                      {movies.poster_path ? (
+                        <div className='movie-list'>
+                          <img
+                            src={`${IMG_URL}${POSTER_SIZE}${movies.poster_path}`}
+                            alt='movie thumb'
+                          />
+                        </div>
+                      ) : null}
+                    </div>
+                  </NavLink>
+                  <Route
+                    exact
+                    path='/movie/:id'
+                    render={(props) => (
+                      <MovieInfo {...props} movie={movies} key={movies.id} />
+                    )}
                   />
                 </div>
               );
@@ -62,17 +84,22 @@ function ActorInfo(props) {
           <h3>Gender</h3>
           <p>{actor.gender === 2 ? 'Male' : 'Female'}</p>
         </div>
-        <div className='actor-data'>
-          <h3>Birthday</h3>
-          <p>
-            {format(actor.birthday)} (
-            {actor.birthday ? calcAge(actor.birthday) : null} years old)
-          </p>
-        </div>
-        <div className='actor-data'>
-          <h3>Place of Birth</h3>
-          <p>{actor.place_of_birth}</p>
-        </div>
+        {actor.birthday ? (
+          <div className='actor-data'>
+            <h3>Birthday</h3>
+            <p>
+              {format(actor.birthday)} (
+              {actor.birthday ? calcAge(actor.birthday) : null} years old)
+            </p>
+          </div>
+        ) : null}
+        {actor.place_of_birth ? (
+          <div className='actor-data'>
+            <h3>Place of Birth</h3>
+            <p>{actor.place_of_birth}</p>
+          </div>
+        ) : null}
+        <ActorMovieList list={actor_movies.cast} />
       </div>
     </div>
   );
---
+++ src/components/ActorMovieList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ActorMovieList(props) {
+  console.log('ACTOR MOVIE LIST', props);
+  return (
+    <div>
+      <h1>Actor Movie List</h1>
+    </div>
+  );
+}
+
+export default ActorMovieList;

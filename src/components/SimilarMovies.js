dd943a77c348980752e4ec127c1db7047e95af3f
Brianna Workman <brianna.workman@hotmail.com>
2020-08-10 14:11:51 -0400

    adds carousel to similar movies

--- src/components/SimilarMovies.js
+++ src/components/SimilarMovies.js
@@ -2,13 +2,70 @@ import React from 'react';
 import MovieThumb from './MovieThumb';
 import { IMG_URL, POSTER_SIZE } from '../utils/config';
 
+import chevronLeft from '../assets/chevron-circle-left-solid.svg';
+import chevronRight from '../assets/chevron-circle-right-solid.svg';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 function SimilarMovies(props) {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1275 },
+      items: 7,
+      slidesToSlide: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1275, min: 464 },
+      items: 4,
+      slidesToSlide: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
+
+  const ButtonGroup = ({ next, previous, goToSlide, ...rest }) => {
+    const {
+      carouselState: { currentSlide },
+    } = rest;
+    return (
+      <div className='similar-carousel-button-group'>
+        <button className='previous-button' />
+        <img
+          className='prev-img'
+          src={chevronLeft}
+          onClick={() => previous()}
+        />
+        <img src={chevronRight} className='next-img' onClick={() => next()} />
+      </div>
+    );
+  };
+
   return (
     <div>
       {props.similar.length > 0 ? (
         <div>
           <h2 className='similar-title'>SIMILAR MOVIES</h2>
-          <div className='movies'>
+          <Carousel
+            swipeable={false}
+            draggable={false}
+            showDots={false}
+            responsive={responsive}
+            ssr={false}
+            infinite={true}
+            autoPlaySpeed={1000}
+            keyBoardControl={true}
+            customTransition='all 1'
+            transitionDuration={1000}
+            containerClass='similar-carousel-container'
+            removeArrowOnDeviceType={['mobile']}
+            itemClass='carousel-item-padding-40-px'
+            arrows={false}
+            renderButtonGroupOutside={true}
+            customButtonGroup={<ButtonGroup />}
+          >
             {props.similar.map((similar) => {
               return (
                 <div key={similar.id}>
@@ -23,7 +80,7 @@ function SimilarMovies(props) {
                 </div>
               );
             })}
-          </div>
+          </Carousel>
         </div>
       ) : null}
       <div className='spacer'></div>

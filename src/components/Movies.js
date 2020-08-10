f74447b6f3a46b7736d2a8ae6518ca635be06ffa
Brianna Workman <brianna.workman@hotmail.com>
2020-08-09 22:43:55 -0400

    finished styling carousel for home movie page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -9,61 +9,72 @@ import MovieInfo from './MovieInfo';
 import MovieThumb from './MovieThumb';
 
 import NoImage from '../assets/no_image.jpg';
+import chevronLeft from '../assets/chevron-circle-left-solid.svg';
+import chevronRight from '../assets/chevron-circle-right-solid.svg';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
 function Movies(props) {
   let data = props.movie_data.data;
-  let trending = undefined;
-  let index = 0;
-
-  if (props.title === 'Trending Today') {
-    trending = props.title;
-  }
 
   const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 10,
-    },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 6,
+      breakpoint: { max: 3000, min: 1275 },
+      items: 7,
+      slidesToSlide: 1,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 5,
+      breakpoint: { max: 1275, min: 464 },
+      items: 4,
+      slidesToSlide: 1,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 2,
+      items: 1,
+      slidesToSlide: 1,
     },
   };
+
+  const ButtonGroup = ({ next, previous, goToSlide, ...rest }) => {
+    const {
+      carouselState: { currentSlide },
+    } = rest;
+    return (
+      <div className='carousel-button-group'>
+        <button className='previous-button' />
+        <img
+          className='prev-img'
+          src={chevronLeft}
+          onClick={() => previous()}
+        />
+        <img src={chevronRight} className='next-img' onClick={() => next()} />
+      </div>
+    );
+  };
+
   return (
     <Carousel
       swipeable={false}
       draggable={false}
       showDots={false}
       responsive={responsive}
-      ssr={true}
+      ssr={false}
       infinite={true}
       autoPlaySpeed={1000}
       keyBoardControl={true}
-      customTransition='all .5'
-      transitionDuration={500}
+      customTransition='all 1'
+      transitionDuration={1000}
       containerClass='carousel-container'
-      removeArrowOnDeviceType={['tablet', 'mobile']}
-      dotListClass='custom-dot-list-style'
+      removeArrowOnDeviceType={['mobile']}
       itemClass='carousel-item-padding-40-px'
+      arrows={false}
+      renderButtonGroupOutside={true}
+      customButtonGroup={<ButtonGroup />}
     >
       {data.map((movieData) => {
         let vote_average = Math.floor((movieData.vote_average / 10) * 100);
-        if (trending) {
-          index = data.indexOf(movieData);
-        }
         return (
           <div key={movieData.id} className='movie-container'>
-            {trending ? <h1 className='trending-count'>{index + 1}</h1> : null}
             <div>
               <div className='poster-container'>
                 <NavLink
@@ -114,16 +125,14 @@ function Movies(props) {
                   />
                 </div>
               </div>
-              {trending ? null : (
-                <div className='movie-data'>
-                  <h3 className='movie-title'>
-                    {movieData.title.length > 15
-                      ? movieData.title.substring(0, 15, 3) + '...'
-                      : movieData.title}
-                  </h3>
-                  <p>{format(movieData.release_date)}</p>
-                </div>
-              )}
+              <div className='movie-data'>
+                <h3 className='movie-title'>
+                  {movieData.title.length > 15
+                    ? movieData.title.substring(0, 15, 3) + '...'
+                    : movieData.title}
+                </h3>
+                <p>{format(movieData.release_date)}</p>
+              </div>
             </div>
           </div>
         );

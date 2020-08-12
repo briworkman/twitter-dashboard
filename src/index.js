8a9523d18f114999ebeca54d433af7fb51495d14
Brianna Workman <brianna.workman@hotmail.com>
2020-08-12 12:59:07 -0400

    fixes scroll to top

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </Provider>,

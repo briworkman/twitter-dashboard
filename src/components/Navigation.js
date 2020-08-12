f51e6543caf38b12ba48ba354cbab98de82cff2c
Brianna Workman <brianna.workman@hotmail.com>
2020-08-12 12:11:38 -0400

    styles navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,13 +4,20 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <div>
-      <Link to={'/'}>
-        <img src={logo} alt='logo' className='logo' />
-      </Link>
-      <Link to={'/movies/search'}>
-        <p>search</p>
-      </Link>
+    <div className='nav-container'>
+      <div className='logo-container'>
+        <Link to={'/'}>
+          <img src={logo} alt='logo' className='logo' />
+        </Link>
+      </div>
+      <div className='nav-links'>
+        <Link to={'/'} className='link'>
+          <p>HOME</p>
+        </Link>
+        <Link to={'/movies/search'} className='link'>
+          <p>SEARCH</p>
+        </Link>
+      </div>
     </div>
   );
 }
